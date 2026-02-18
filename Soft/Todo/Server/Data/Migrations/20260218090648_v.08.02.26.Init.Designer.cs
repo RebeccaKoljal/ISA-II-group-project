5d9cba80d5add2880b4576839b1e7045bee1158f
Rebecca Koljal <rebeck@taltech.ee>
2026-02-18 11:08:27 +0200

    Added migration to for the database

--- Soft/Todo/Server/Data/Migrations/20260218090648_v.08.02.26.Init.Designer.cs
+++ Soft/Todo/Server/Data/Migrations/20260218090648_v.08.02.26.Init.Designer.cs
@@ -4,6 +4,7 @@ using Abc.Soft.Todo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abc.Soft.Todo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260218090648_v.08.02.26.Init")]
+    partial class v080226Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
