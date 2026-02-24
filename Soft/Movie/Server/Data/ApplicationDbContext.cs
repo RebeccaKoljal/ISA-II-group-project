b064d5cf78ba8232eb20b67595c38ad73aef5a16
Rebecca Koljal <rebeck@taltech.ee>
2026-02-24 02:15:13 +0200

    Removed one DbContext to only have one

--- Soft/Movie/Server/Data/ApplicationDbContext.cs
+++ Soft/Movie/Server/Data/ApplicationDbContext.cs
@@ -5,5 +5,6 @@ namespace Abc.Soft.Web.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Abc.Soft.Web.Model.Movie> Movie { get; set; } = default!;
     }
 }
