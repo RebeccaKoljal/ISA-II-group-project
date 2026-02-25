bdddb3fd62942f506b4246953246a85ef2c314b1
Rebecca Koljal <rebeck@taltech.ee>
2026-02-25 11:08:32 +0200

    Added POCO's to Country

--- Code/Data/Country.cs
+++ Code/Data/Country.cs
@@ -2,6 +2,12 @@
 {
     public class Country
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string CountryCode { get; set; }
+        public int TotalArea { get; set; }
+        public int PopulationSize { get; set; }
 
     }
 }
