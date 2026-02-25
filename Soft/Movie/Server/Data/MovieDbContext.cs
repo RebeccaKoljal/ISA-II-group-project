0ce7f517e14645eae062cbab7750cef00ca7c641
Rebecca Koljal <rebeck@taltech.ee>
2026-02-25 10:06:54 +0200

    Fixed login error and renamed database

--- Soft/Movie/Server/Data/MovieDbContext.cs
+++ Soft/Movie/Server/Data/MovieDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Abc.Soft.Web.Data
 {
-    public class MovieDbContext(DbContextOptions<MovieDbContext> options) : IdentityDbContext(options)
+    public class MovieDbContext(DbContextOptions<MovieDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Abc.Soft.Web.Model.Movie> Movie { get; set; } = default!;
     }
--- Soft/Movie/Server/Migrations/20260225080226_ApplicationUser.Designer.cs
+++ Soft/Movie/Server/Migrations/20260225080226_ApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using Abc.Soft.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abc.Soft.Web.Migrations
 {
     [DbContext(typeof(MovieDbContext))]
-    partial class MovieDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225080226_ApplicationUser")]
+    partial class ApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
