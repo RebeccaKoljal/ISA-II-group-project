7d9e83dc078b7be9a6ae34d579d0c58969689934
Rebecca Koljal <rebeck@taltech.ee>
2026-03-04 10:16:35 +0200

    Added a test, that checks if the right thing is tested

--- Tests/Tests.Data/BaseTests.cs
+++ Tests/Tests.Data/BaseTests.cs
@@ -5,5 +5,17 @@
         private TClass obj;
         [TestInitialize] public void Initialize() => obj = new TClass();
         [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
+        [TestMethod] public void IsClassCorrectTest() // checks if the test class is named correctly according to the convention
+        {
+            var className = typeof(TClass).Name;
+            var testClassName = GetType().Name;
+            Assert.AreEqual(testClassName.Replace("Tests", ""), className); 
+        }
+        [TestMethod] public void IsClassTestedTest() 
+        {
+            var testMethods = GetType().GetMethods();
+            var classMembers = typeof(TClass).GetMembers();
+            Assert.Inconclusive("Not all members are tested."); 
+        }
     }
 }
