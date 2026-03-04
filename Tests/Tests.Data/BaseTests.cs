5eda37f95c2333f459008bfe158f24149452e614
Rebecca Koljal <rebeck@taltech.ee>
2026-03-04 10:37:34 +0200

    Added a test, that says, if everything is tested

--- Tests/Tests.Data/BaseTests.cs
+++ Tests/Tests.Data/BaseTests.cs
@@ -1,11 +1,17 @@
-﻿namespace Abc.Tests.Data
+﻿using System.Reflection;
+
+namespace Abc.Tests.Data
 {
     public abstract class BaseTests<TClass> where TClass : class, new()
     {
         private TClass obj;
+        private const BindingFlags publicDeclared = BindingFlags.Public 
+            | BindingFlags.Instance 
+            | BindingFlags.DeclaredOnly
+            | BindingFlags.Static;
         [TestInitialize] public void Initialize() => obj = new TClass();
         [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
-        [TestMethod] public void IsClassCorrectTest() // checks if the test class is named correctly according to the convention
+        [TestMethod] public void IsCorrectClassTest() // checks if the test class is named correctly according to the convention
         {
             var className = typeof(TClass).Name;
             var testClassName = GetType().Name;
@@ -13,9 +19,21 @@
         }
         [TestMethod] public void IsClassTestedTest() 
         {
-            var testMethods = GetType().GetMethods();
-            var classMembers = typeof(TClass).GetMembers();
-            Assert.Inconclusive("Not all members are tested."); 
+            var testMethods = GetType().GetMethods().Select(x => x.Name);
+            var membersToTest = getProperties().Concat(getMethods());
+            foreach (var m in membersToTest)
+            {
+                if (!testMethods.Contains(m + "Test"))
+                    Assert.Inconclusive($"{m} is not tested");
+            }
         }
+
+        private static IEnumerable<string> getProperties() => typeof(TClass)
+            .GetProperties(publicDeclared)
+            .Select(i => i.Name); // i as in info
+        private static IEnumerable<string> getMethods() => Array.FindAll(
+            typeof(TClass).GetMethods(publicDeclared),
+            i => !i.IsSpecialName)
+            .Select(i => i.Name);
     }
 }
