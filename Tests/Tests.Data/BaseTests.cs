8e2b21cfde49634831351d126d38f748a78d354b
Rebecca Koljal <rebeck@taltech.ee>
2026-03-04 09:59:56 +0200

    Moved all tests to their own namespace

---
+++ Tests/Tests.Data/BaseTests.cs
@@ -0,0 +1,9 @@
+﻿namespace Abc.Tests.Data
+{
+    public abstract class BaseTests<TClass> where TClass : class, new()
+    {
+        private TClass obj;
+        [TestInitialize] public void Initialize() => obj = new TClass();
+        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
+    }
+}
--- Tests/Tests.Data/MovieTests.cs
+++ Tests/Tests.Data/MovieTests.cs
@@ -2,27 +2,5 @@
 
 namespace Abc.Tests.Data
 {
-    [TestClass]
-    public sealed class MovieTests
-    {
-        private Movie movie;
-        [TestInitialize] public void Initialize() => movie = new Movie();
-        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(movie);
-    }
-
-    [TestClass]
-    public sealed class CountryTests
-    {
-        private Country movie;
-        [TestInitialize] public void Initialize() => movie = new Country();
-        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(movie);
-    }
-
-    [TestClass]
-    public sealed class CurrencyTests
-    {
-        private Currency movie;
-        [TestInitialize] public void Initialize() => movie = new Currency();
-        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(movie);
-    }
+    [TestClass] public sealed class MovieTests : BaseTests<Movie> { }
 }
