ff765b93e2888dc0b0357d916257ba2b242aba0d
Rebecca Koljal <rebeck@taltech.ee>
2026-03-05 22:44:32 +0200

    Changed base tests a bit and made test aids

--- Tests/Tests.Data/BaseTests.cs
+++ Tests/Tests.Data/BaseTests.cs
@@ -1,24 +1,19 @@
 ﻿using Abc.Aids;
-using System.Reflection;
+using System.Formats.Asn1;
 
 namespace Abc.Tests.Data
 {
-    public abstract class BaseTests<TClass> where TClass : class, new()
+    public abstract class BaseTests<TClass> : TestAids<TClass> where TClass : class, new() // deals with tests
     {
-        private TClass obj;
-        private const BindingFlags publicDeclared = BindingFlags.Public 
-            | BindingFlags.Instance 
-            | BindingFlags.DeclaredOnly
-            | BindingFlags.Static;
         [TestInitialize] public void Initialize() => obj = new TClass();
         [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
         [TestMethod] public void IsCorrectClassTest() // checks if the test class is named correctly according to the convention
         {
             var className = typeof(TClass).Name;
             var testClassName = GetType().Name;
-            Assert.AreEqual(testClassName.Replace("Tests", ""), className); 
+            Assert.AreEqual(testClassName.Replace("Tests", ""), className);
         }
-        [TestMethod] public void IsClassTestedTest() 
+        [TestMethod] public void IsClassTestedTest()
         {
             var testMethods = GetType().GetMethods().Select(x => x.Name);
             var membersToTest = GetProperties().Concat(GetMethods());
@@ -28,11 +23,5 @@ namespace Abc.Tests.Data
                     Assert.Inconclusive($"{m} is not tested");
             }
         }
-
-        private static IEnumerable<string> GetProperties() 
-            => Aids.GetType.PropertyNames<TClass>(publicDeclared);
-        private static IEnumerable<string> GetMethods() 
-            => Aids.GetType.MethodNames<TClass>(publicDeclared, false);
-
     }
 }
---
+++ Tests/Tests.Data/TestAids.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Abc.Tests.Data
+{
+    public abstract class TestAids<TClass> where TClass : class, new() // provides helper methods for the tests
+    {
+        protected TClass obj;
+        protected const BindingFlags publicDeclared = BindingFlags.Public
+            | BindingFlags.Instance
+            | BindingFlags.DeclaredOnly
+            | BindingFlags.Static;
+
+        protected static IEnumerable<string> GetProperties()
+            => Aids.GetType.PropertyNames<TClass>(publicDeclared);
+        protected static IEnumerable<string> GetMethods()
+            => Aids.GetType.MethodNames<TClass>(publicDeclared, false);
+    }
+}
