583e5c350352335d14667004e37fac3fb1839e7b
Rebecca Koljal <rebeck@taltech.ee>
2026-03-07 21:58:55 +0200

    Changed all Movie tests from inconlucise to their own properties

--- Tests/Tests.Data/MovieTests.cs
+++ Tests/Tests.Data/MovieTests.cs
@@ -5,10 +5,10 @@ namespace Abc.Tests.Data
     [TestClass] public sealed class MovieTests : BaseTests<Movie> 
     {
         [TestMethod] public void IdTest() => IsProperty<int>(nameof(Movie.Id));
-        [TestMethod] public void TitleTest() => Assert.Inconclusive();
-        [TestMethod] public void ReleaseDateTest() => Assert.Inconclusive();
-        [TestMethod] public void GenreTest() => Assert.Inconclusive();
-        [TestMethod] public void PriceTest() => Assert.Inconclusive();
+        [TestMethod] public void TitleTest() => IsProperty<string>(nameof(Movie.Title));
+        [TestMethod] public void ReleaseDateTest() => IsProperty<DateOnly>(nameof(Movie.ReleaseDate));
+        [TestMethod] public void GenreTest() => IsProperty<string>(nameof(Movie.Genre));
+        [TestMethod] public void PriceTest() => IsProperty<decimal>(nameof(Movie.Price));
 
     }
 }
