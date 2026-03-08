152d02615e4e2719fc885fff625c88fb784c8d02
Rebecca Koljal <rebeck@taltech.ee>
2026-03-08 14:02:06 +0200

    Added tests for Country, but two are not doable

--- Code/Data/Country.cs
+++ Code/Data/Country.cs
@@ -6,7 +6,7 @@ namespace Abc.Data
     {
         public string OfficialName { get; set; } = "";
         public string NativeName { get; set; } = "";
-        public string NumericCOde { get; set; } = "";
+        public string NumericCode { get; set; } = "";
         bool IsIsoCountry { get; set; }
         bool IsLoyaltyProgram { get; set; }
         public string IsoCode { get; set; } = "";
