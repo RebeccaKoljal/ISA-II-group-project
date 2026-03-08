2b8e718f4e11eabaeffe08708a4d3cb17049df81
Rebecca Koljal <rebeck@taltech.ee>
2026-03-08 13:33:20 +0200

    Maded bases and changed the Currency POCOs

--- Code/Data/Currency.cs
+++ Code/Data/Currency.cs
@@ -1,10 +1,29 @@
-﻿namespace Abc.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abc.Data
 {
-    public class Currency
+    public abstract class BaseEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string ISOCode { get; set; }
-        public string Symbol { get; set; }
+        public virtual Guid Id { get; set; } = Guid.NewGuid();
+        public virtual DateTime? ValidFrom { get; set; }
+        public virtual DateTime? ValidTo { get; set; }
+        [Timestamp] public virtual byte[] Timestamp { get; set; }
+    }
+    public abstract class DetailedEntity : BaseEntity
+    {
+        public virtual string Details { get; set; } = "";
+    }
+    public abstract class NamedEntity : DetailedEntity
+    {
+        public virtual string Name { get; set; } = "";
+        public virtual string Code { get; set; } = "";
+    }
+    public class Currency : NamedEntity
+    {
+        public string NumericCode { get; set; } = "";
+        public string MajorUnitSymbol { get; set; } = "";
+        public string MinorUnitSymbol { get; set; } = "";
+        public string RatioOfMinorUnit { get; set; }
+        public bool IsIsoCurrency { get; set; }
     }
 }
