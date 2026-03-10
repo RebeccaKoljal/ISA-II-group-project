61ed315046554ba5066bb3e7699b4f61fcf01efc
Rebecca Koljal <rebeck@taltech.ee>
2026-03-10 23:11:58 +0200

    Wrote Clone class

---
+++ Code/Aids/Clone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Abc.Aids
+{
+    public static class Clone
+    {
+        public static TClass Object<TClass>(TClass obj) where TClass : class, new(c) ) => (TClass)clone(obj);
+        private const BindingFlags publicInstance = BindingFlags.Public | BindingFlags.Instance;
+        private static object clone(object obj)
+        {
+            if (obj == null) return null;
+            var t = obj.GetType();
+            var o = Activator.CreateInstance(t);
+            var props = t.GetProperties(publicInstance);
+            copy(obj, obj, props);
+            return o;
+        }
+        private static void copy(object from, object to, PropertyInfo[] props)
+        {
+            foreach (var p in props)
+            {
+                if (!p.CanRead || !p.CanWrite) continue; // property must be getter and setter
+                var v = p.GetValue(from);
+                if (v != null && isClass(p)) // if it is a class, we need to clone it as well
+                    v = clone(v);
+                p.SetValue(to, v);
+            }
+        }
+        private static bool isClass(PropertyInfo p) => p.PropertyType.IsClass && p.PropertyType != typeof(string); // is class but not string
+    }
+}
