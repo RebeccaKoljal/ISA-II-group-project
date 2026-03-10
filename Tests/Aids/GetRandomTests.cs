2ae0d32ca54eb73c92da4dea5719558b9ffa20a3
Rebecca Koljal <rebeck@taltech.ee>
2026-03-10 23:49:03 +0200

    Added tests for GetRandom

---
+++ Tests/Aids/GetRandomTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Aids;
+
+namespace Abc.Tests.Aids
+{
+    [TestClass] public sealed class GetRandomTests
+    {
+        private const sbyte min = sbyte.MinValue; // so that there can be negative values as well
+        private const sbyte max = sbyte.MaxValue;
+        [TestMethod] public void Int32Test() => Assert.AreNotEqual(GetRandom.Int32(min, max), GetRandom.Int32(min, max));
+        [TestMethod] public void Int64Test() => Assert.AreNotEqual(GetRandom.Int64(min, max), GetRandom.Int64(min, max));
+        [TestMethod] public void DoubleTest() => Assert.AreNotEqual(GetRandom.Double(min, max), GetRandom.Double(min, max));
+    }
+}
