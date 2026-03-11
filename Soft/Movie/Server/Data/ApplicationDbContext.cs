5827d32bc324b914dd8fa08e44bdfd420124f879
Rebecca Koljal <rebeck@taltech.ee>
2026-03-11 10:26:39 +0200

    Preparations to add Country

--- Soft/Movie/Server/Data/ApplicationDbContext.cs
+++ Soft/Movie/Server/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace Abc.Soft.Web.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        public DbSet<Movie> Movie { get; set; } = default!;
+        public DbSet<Movie> Movies { get; set; } = default!;
+        public DbSet<Country> Countries { get; set; } = default!;
     }
 }
---
+++ Soft/Movie/Server/Migrations/20260311081709_CountriesInitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abc.Soft.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountriesInitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie");
+
+            migrationBuilder.RenameTable(
+                name: "Movie",
+                newName: "Movies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OfficialName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NativeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumericCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsoCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ValidFrom = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ValidTo = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Timestamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies");
+
+            migrationBuilder.RenameTable(
+                name: "Movies",
+                newName: "Movie");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie",
+                column: "Id");
+        }
+    }
+}
