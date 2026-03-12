5cc340c24ce28c740dd1555cc133b54371fa4d81
Rebecca Koljal <rebeck@taltech.ee>
2026-03-12 23:58:38 +0200

    Added Movie class to inherit from base classes and fixed all the errors that came with it. Now problems with migration

---
+++ Soft/Movie/Server/Migrations/20260312215207_Movies.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abc.Soft.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Movies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Movies",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Movies",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "Movies",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidFrom",
+                table: "Movies",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidTo",
+                table: "Movies",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ValidFrom",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ValidTo",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Movies",
+                newName: "Title");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+    }
+}
