dbdb1475e8eeb6f5aa3238907c172a472791e770
Rebecca Koljal <rebeck@taltech.ee>
2026-03-17 20:11:16 +0200

    GetRandom and the tests are all done except error in object class

--- Code/Aids/GetRandom.cs
+++ Code/Aids/GetRandom.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection;
+
 namespace Abc.Aids;
 
 public static class GetRandom
@@ -32,5 +34,72 @@ public static class GetRandom
         var minLong = (long) min - long.MaxValue;
         var maxLong = (long) max - long.MaxValue;
         return (ulong) Int64(minLong, maxLong) + long.MaxValue;
+    }
+    public static float Float(float min = float.MinValue, float max = float.MaxValue) => (float) Double(min, max);
+    public static decimal Decimal(decimal min = decimal.MinValue, decimal max = decimal.MaxValue) => (decimal) Double((double) min, (double) max);
+    public static string String(byte minLength = byte.MinValue, byte maxLength = (byte) sbyte.MaxValue)
+    {
+        var len = UInt8(minLength, maxLength);
+        var s = new char[len];
+        for (var i = 0; i < len; i++) s[i] = Char('a', 'z');
+        return new string(Char('a', 'z'), len);
+    }
+    public static char Char(char min, char max) => (char)UInt16(min, max);
+    public static bool Bool() => r.Next(2) == 0;
+    public static DateTime DateTime(System.DateTime? min = null, System.DateTime? max = null)
+    {
+        var minTicks = min?.Ticks?? System.DateTime.MinValue.Ticks;
+        var maxTicks = max?.Ticks?? System.DateTime.MaxValue.Ticks;
+        var ticks = Int64(minTicks, maxTicks);
+        return new DateTime(ticks);
+    }
+    public static TimeSpan TimeSpan(System.TimeSpan? min = null, System.TimeSpan? max = null)
+    {
+        var minTicks = min?.Ticks?? System.TimeSpan.MinValue.Ticks;
+        var maxTicks = max?.Ticks?? System.TimeSpan.MaxValue.Ticks;
+        var ticks = Int64(minTicks, maxTicks);
+        return new TimeSpan(ticks);
+    }
+    public static Guid Guid()
+    {
+        Span<byte> buffer = stackalloc byte[16];
+        r.NextBytes(buffer);
+        return new Guid(buffer);
+    }
+    public static object Object(Type t)
+    {
+        var x = Nullable.GetUnderlyingType(t);
+        if (x is not null) t = x;
+        var o = Activator.CreateInstance(t);
+        foreach(var p in t.GetProperties())
+        {
+            if (!p.CanWrite) continue;
+            if (p.PropertyType.IsArray) continue;
+            var v = isClass(p) ? Object(p.PropertyType) : Value(p.PropertyType);
+            p.SetValue(o, v);
+        }
+        return o;
+    }
+    private static bool isClass(PropertyInfo p) => p.PropertyType.IsClass && p.PropertyType != typeof(string);
+    private static object Value(Type t)
+    {
+        if (t == typeof(sbyte)) return Int8();
+        if (t == typeof(short)) return Int16();
+        if (t == typeof(int)) return Int32();
+        if (t == typeof(long)) return Int64();
+        if (t == typeof(byte)) return UInt8();
+        if (t == typeof(ushort)) return UInt16();
+        if (t == typeof(uint)) return UInt32();
+        if (t == typeof(ulong)) return UInt64();
+        if (t == typeof(float)) return Float();
+        if (t == typeof(double)) return Double();
+        if (t == typeof(decimal)) return Decimal();
+        if (t == typeof(string)) return String();
+        if (t == typeof(char)) return Char((char) 0, char.MaxValue);
+        if (t == typeof(bool)) return Bool();
+        if (t == typeof(DateTime)) return DateTime();
+        if (t == typeof(TimeSpan)) return TimeSpan();
+        if (t == typeof(Guid)) return Guid();
+        throw new NotSupportedException($"Type {t} is not supported");
     }
 }
--- Tests/Aids/GetRandomTests.cs
+++ Tests/Aids/GetRandomTests.cs
@@ -1,4 +1,5 @@
 ﻿using Abc.Aids;
+using Abc.Data.Common;
 
 namespace Abc.Tests.Aids;
 
@@ -15,4 +16,44 @@ namespace Abc.Tests.Aids;
     [TestMethod] public void UInt32Test() => Assert.AreNotEqual(GetRandom.UInt32(0, (uint)max), GetRandom.UInt32(0, (uint)max));
     [TestMethod] public void UInt64Test() => Assert.AreNotEqual(GetRandom.UInt64(0, (ulong)max), GetRandom.UInt64(0, (ulong)max));
     [TestMethod] public void DoubleTest() => Assert.AreNotEqual(GetRandom.Double(min, max), GetRandom.Double(min, max));
+    [TestMethod] public void FloatTest() => Assert.AreNotEqual(GetRandom.Float(min, max), GetRandom.Float(min, max));
+    [TestMethod] public void DecimalTest() => Assert.AreNotEqual(GetRandom.Decimal(min, max), GetRandom.Decimal(min, max));
+    [TestMethod] public void StringTest() => Assert.AreNotEqual(GetRandom.String(0, (byte) max), GetRandom.String(0, (byte) max));
+    [TestMethod] public void CharTest() => Assert.AreNotEqual(GetRandom.Char((char) 0, (char) max), GetRandom.Char((char) 0, (char) max));
+    [TestMethod]
+    public void BoolTest()
+    {
+        var x = GetRandom.Bool();
+        bool y = GetRandom.Bool();
+        for (var i = 0; i < 10; i++)
+        {
+            if (x != y) break;
+            y = GetRandom.Bool();
+        }
+        Assert.AreNotEqual(x, y);
+    }
+    [TestMethod] public void DateTimeTest() 
+    { 
+        var minDate = DateTime.Now.AddYears(-100);
+        var maxDate = DateTime.Now.AddYears(100);
+        Assert.AreNotEqual(GetRandom.DateTime(minDate, maxDate), GetRandom.DateTime(minDate, maxDate)); 
+    }
+    [TestMethod] public void TimeSpanTest()  
+    {
+        var minSpan = TimeSpan.FromTicks(DateTime.Now.AddYears(-100).Ticks);
+        var maxSpan = TimeSpan.FromTicks(DateTime.Now.AddYears(100).Ticks);
+        Assert.AreNotEqual(GetRandom.TimeSpan(minSpan, maxSpan), GetRandom.TimeSpan(minSpan, maxSpan)); 
+    }
+    [TestMethod] public void GuidTest() => Assert.AreNotEqual(GetRandom.Guid(), GetRandom.Guid());
+    private class testClass : NamedEntity { }
+    [TestMethod] public void ObjectTest()
+    {
+        var o1 = GetRandom.Object(typeof(testClass));
+        var o2 = GetRandom.Object(typeof(testClass));
+        foreach (var p in typeof(testClass).GetProperties())
+        {
+            if (p.PropertyType.IsArray) continue;
+            Assert.AreNotEqual(p.GetValue(o1), p.GetValue(o2));
+        }
+    }
 }
