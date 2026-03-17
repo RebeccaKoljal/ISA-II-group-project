0a1d0b37e0c1123d4cd35094f772c43d5bb11b85
Rebecca Koljal <rebeck@taltech.ee>
2026-03-17 17:51:34 +0200

    Added IdTest in movie tests

--- Tests/Tests.Data/MovieTests.cs
+++ Tests/Tests.Data/MovieTests.cs
@@ -5,6 +5,7 @@ namespace Abc.Tests.Data;
 [TestClass]
 public sealed class MovieTests : BaseTests<Movie>
 {
+    [TestMethod] public void IdTest() => IsProperty<Guid>(nameof(Movie.Id));
     [TestMethod] public void TitleTest() => IsProperty<string>(nameof(Movie.Name));
     [TestMethod] public void ReleaseDateTest() => IsProperty<DateTime?>(nameof(Movie.ValidFrom));
     [TestMethod] public void GenreTest() => IsProperty<string>(nameof(Movie.Genre));
