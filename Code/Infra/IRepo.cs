e8f48d1d0d6b6a5c04d5eb8d8cf1a2577bd4fd0b
Rebecca Koljal <rebeck@taltech.ee>
2026-03-17 20:50:44 +0200

    Added IRepo interface and base for Repo

---
+++ Code/Infra/IRepo.cs
@@ -0,0 +1,18 @@
+﻿using Abc.Data;
+using Abc.Data.Common;
+
+namespace Abc.Infra
+{
+    public interface IRepo<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetAsync(Guid id); 
+        Task<IEnumerable<TEntity>> GetAsync(); // search for all entities and gives the info back
+        Task<TEntity> CreateAsync(TEntity e); // creates the database
+        Task<TEntity> UpdateAsync(TEntity e); 
+        Task DeleteAsync(Guid id);
+    }
+    public interface IMoviesRepo : IRepo<Movie> {}
+    public interface ICountriesRepo : IRepo<Country> {}
+    public interface ICurrenciesRepo : IRepo<Currency> {}
+
+}
