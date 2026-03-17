0498f0ab0e280cc549d6e48ec4f541187aed19fe
Rebecca Koljal <rebeck@taltech.ee>
2026-03-17 21:22:37 +0200

    Added Movie, Country and Currency repos and then fixed all mistakes thatcame with it

---
+++ Code/Infra/Repos.cs
@@ -0,0 +1,9 @@
+﻿using Abc.Data;
+
+
+namespace Abc.Infra
+{
+    public class MoviesRepos (ApplicationDbContext c = null): EfBaseRepo<ApplicationDbContext, Movie> (c), IMoviesRepo { }
+    public class CurrencyRepos (ApplicationDbContext c = null) : EfBaseRepo<ApplicationDbContext, Currency>(c), ICurrenciesRepo { }
+    public class CountriesRepos (ApplicationDbContext c = null) : EfBaseRepo<ApplicationDbContext, Country>(c), ICountriesRepo { }
+}
