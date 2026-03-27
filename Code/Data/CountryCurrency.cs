688643f3e8e51f669e70a078c9574adf0d7dfe10
Rebecca Koljal <rebeck@taltech.ee>
2026-03-27 21:28:58 +0200

    Added a diagram and added new classes

---
+++ Code/Data/CountryCurrency.cs
@@ -0,0 +1,15 @@
+﻿using Abc.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abc.Data
+{
+    public class CountryCurrency : DetailedEntity
+    {
+        public Guid CountryId { get; set; }
+        public Guid CurrencyId { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
---
+++ Code/Data/Money.cs
@@ -0,0 +1,15 @@
+﻿using Abc.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abc.Data
+{
+    public class Money : BaseEntity
+    {
+        public decimal Amount { get; set; }
+        public Guid CurrencyId { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
