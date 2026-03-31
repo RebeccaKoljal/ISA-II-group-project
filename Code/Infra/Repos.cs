19e1c2ce6bdf8ca439033fb145f61858b1bf0f98
Rebecca Koljal <rebeck@taltech.ee>
2026-03-31 23:22:37 +0300

    Added repos for Money and CountryCurrency. Infra is done

--- Code/Infra/Repos.cs
+++ Code/Infra/Repos.cs
@@ -1,9 +1,10 @@
 ﻿using Abc.Data;
 
-
 namespace Abc.Infra
 {
     public class MoviesRepo (ApplicationDbContext c = null): EfBaseRepo<ApplicationDbContext, Movie> (c), IMoviesRepo { }
     public class CurrenciesRepo (ApplicationDbContext c = null) : EfBaseRepo<ApplicationDbContext, Currency>(c), ICurrenciesRepo { }
     public class CountriesRepo (ApplicationDbContext c = null) : EfBaseRepo<ApplicationDbContext, Country>(c), ICountriesRepo { }
+    public class MoniesRepo (ApplicationDbContext c = null) : EfBaseRepo<ApplicationDbContext, Money>(c), IMoniesRepo { }
+    public class CountryCurrenciesRepo (ApplicationDbContext c = null) : EfBaseRepo<ApplicationDbContext, CountryCurrency>(c), ICountryCurrenciesRepo { }
 }
