682dbfef6c2d84258a5505202fe2f2c002d66c2e
Rebecca Koljal <rebeck@taltech.ee>
2026-03-31 23:30:02 +0300

    Fixed CurrencyTest cause RatioOfMinorUnitTest was changed from string to double

--- Tests/Tests.Data/CurrencyTests.cs
+++ Tests/Tests.Data/CurrencyTests.cs
@@ -8,6 +8,6 @@ public sealed class CurrencyTests : BaseTests<Currency>
     [TestMethod] public void NumericCodeTest() => IsProperty<string>(nameof(Currency.NumericCode));
     [TestMethod] public void MajorUnitSymbolTest() => IsProperty<string>(nameof(Currency.MajorUnitSymbol));
     [TestMethod] public void MinorUnitSymbolTest() => IsProperty<string>(nameof(Currency.MinorUnitSymbol));
-    [TestMethod] public void RatioOfMinorUnitTest() => IsProperty<string>(nameof(Currency.RatioOfMinorUnit));
+    [TestMethod] public void RatioOfMinorUnitTest() => IsProperty<double>(nameof(Currency.RatioOfMinorUnit));
     [TestMethod] public void IsIsoCurrencyTest() => IsProperty<bool>(nameof(Currency.IsIsoCurrency));
 }
