44f6fe45f8e0a32427b9ec0e43801932e98cfe53
Rebecca Koljal <rebeck@taltech.ee>
2026-03-31 23:07:21 +0300

    Added DbSets to Money and CountryCurrencies

--- Code/Infra/ApplicationDbContext.cs
+++ Code/Infra/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace Abc.Infra
         public DbSet<Movie> Movies { get; set; } = default!;
         public DbSet<Country> Countries { get; set; } = default!;
         public DbSet<Currency> Currencies { get; set; } = default!;
+        public DbSet<Money> Monies { get; set; } = default!;
+        public DbSet<CountryCurrency> CountryCurrencies { get; set; } = default!;
     }
 }
