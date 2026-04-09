467179ed50d8beaa39ee08d077d5cbadcc09f95d
Rebecca Koljal <rebeck@taltech.ee>
2026-04-09 23:07:19 +0300

    Added seeding from 10 loeng. Did the base for domain

--- Code/Infra/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Code/Infra/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -218,6 +218,95 @@ namespace Abc.Infra.Migrations
                     b.ToTable("Movies");
                 });
 
+            modelBuilder.Entity("Abc.Data.Product", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Barcode")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime?>("CachedAt")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Code")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Details")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("ImageUrl")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Name")
+                        .HasColumnType("TEXT");
+
+                    b.Property<byte[]>("Timestamp")
+                        .IsConcurrencyToken()
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasColumnType("BLOB");
+
+                    b.Property<DateTime?>("ValidFrom")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime?>("ValidTo")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("Abc.Data.ProductType", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Code")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Details")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Name")
+                        .HasColumnType("TEXT");
+
+                    b.Property<byte[]>("Timestamp")
+                        .IsConcurrencyToken()
+                        .ValueGeneratedOnAddOrUpdate()
+                        .HasColumnType("BLOB");
+
+                    b.Property<DateTime?>("ValidFrom")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime?>("ValidTo")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("ProductTypes");
+                });
+
+            modelBuilder.Entity("Abc.Data.TypeOfProduct", b =>
+                {
+                    b.Property<Guid>("ProductId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<Guid>("ProductTypeId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime?>("AssignedAt")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("ProductId", "ProductTypeId");
+
+                    b.HasIndex("ProductTypeId");
+
+                    b.ToTable("TypeOfProduct");
+                });
+
             modelBuilder.Entity("Abc.Infra.ApplicationUser", b =>
                 {
                     b.Property<string>("Id")
@@ -469,6 +558,25 @@ namespace Abc.Infra.Migrations
                     b.Navigation("Money");
                 });
 
+            modelBuilder.Entity("Abc.Data.TypeOfProduct", b =>
+                {
+                    b.HasOne("Abc.Data.Product", "Product")
+                        .WithMany("TypeOfProduct")
+                        .HasForeignKey("ProductId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Abc.Data.ProductType", "ProductType")
+                        .WithMany("TypeOfProduct")
+                        .HasForeignKey("ProductTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Product");
+
+                    b.Navigation("ProductType");
+                });
+
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
                 {
                     b.HasOne("Microsoft.AspNetCore.Identity.IdentityRole", null)
@@ -574,6 +682,16 @@ namespace Abc.Infra.Migrations
             modelBuilder.Entity("Abc.Data.Country", b =>
                 {
                     b.Navigation("Currencies");
+                });
+
+            modelBuilder.Entity("Abc.Data.Product", b =>
+                {
+                    b.Navigation("TypeOfProduct");
+                });
+
+            modelBuilder.Entity("Abc.Data.ProductType", b =>
+                {
+                    b.Navigation("TypeOfProduct");
                 });
 #pragma warning restore 612, 618
         }
