79bdcbfb52aa40ff4af4f8f615d7aa0566960fbc
Rebecca Koljal <rebeck@taltech.ee>
2026-04-22 11:18:28 +0300

    Added selection for countrycurrency and made base for it

---
+++ Code/Aids/SelectAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Abc.Aids
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class SelectAttribute(Type t, string displayProperty = null) : Attribute
+    {
+        public Type EntityType { get; } = t;
+        public string displayProperty { get; } = string.IsNullOrEmpty(displayProperty) ? "Name" : displayProperty;
+    }
+}
