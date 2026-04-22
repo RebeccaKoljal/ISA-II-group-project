093684182f97f0baa33b1eac711068e12774566d
Rebecca Koljal <rebeck@taltech.ee>
2026-04-22 13:36:03 +0300

    Refactoring

---
+++ Code/Infra/Query.cs
@@ -0,0 +1,23 @@
+﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Abc.Infra
+{
+    public sealed class Query(Dictionary<string, string> d = null)
+    {
+        public static int[] PageSizes => [7, 15, 25, 50, 100];
+        public int Page => toInt(get(nameof(Page)), 1);
+        public int PageSize => toInt(get(nameof(PageSize)), PageSizes[0]);
+        public string SortBy => get(nameof(SortBy));
+        public string SortDir => get(nameof(SortDir));
+        public string SearchBy => get(nameof(SearchBy));
+        public string Selected => get(nameof(Selected));
+        public string SearchStr => get(nameof(SearchStr));
+        private string get(string s) => (d ?? []).TryGetValue(s, out var x) ? x : null;
+        private static int toInt(string s, int def) => int.TryParse(s, out var i) ? i : def;
+        private string sort => string.IsNullOrEmpty(SortBy) ? string.Empty : $"&SortBy={SortBy}&SortDir={SortDir}";
+        private string search => string.IsNullOrEmpty(SearchStr) ? string.Empty : $"&SearchBy={SearchBy}&SearchStr={SearchStr}";
+        public string Href(string baseUri, int? page = null, int? pageSize = null) => $"{baseUri}?Page={page ?? Page}&PageSize={pageSize ?? PageSize}{sort}{search}";
+        public string Href(string baseUri, Guid id) => Href(baseUri) + $"&Select={id}";
+
+    }
+}
