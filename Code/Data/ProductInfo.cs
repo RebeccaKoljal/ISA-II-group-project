238f45bb2c4fb002604e1e57d2a67409d4dd40c9
Rebecca Koljal <rebeck@taltech.ee>
2026-05-12 15:00:47 +0300

    Small fixes in ProductInfo

--- Code/Data/ProductInfo.cs
+++ Code/Data/ProductInfo.cs
@@ -7,13 +7,13 @@ public class ProductInfo : NamedEntity
 {
     [DisplayName("Product ID")] public override Guid Id { get; set; }
     public string Barcode { get; set; } // FK to Barcode
-    [DisplayName("Product")] public override string Name { get; set; }
+    [DisplayName("Product name")] public override string Name { get; set; }
     public string Brand { get; set; }
     [DisplayName("Description")] public override string Details { get; set; }
     public string ImageUrl { get; set; }
     [DisplayName("Expiry Date")] public override DateTime? ValidTo { get; set; }
-    public int CategoryId { get; set; } // FK to Category
+    public Guid CategoryId { get; set; } // FK to Category NEED TO REPLACE WHEN CATEGORY IS DONE IG
     public string WeightOrVolume { get; set; }
-    [DisplayName("Created At")] public override DateTime? ValidFrom { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    [DisplayName("Created at")] public override DateTime? ValidFrom { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
