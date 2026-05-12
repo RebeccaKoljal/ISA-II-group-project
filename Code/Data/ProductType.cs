17c6a3383a04782428720d558fae9d9f798e95da
Rebecca Koljal <rebeck@taltech.ee>
2026-05-12 14:33:17 +0300

    Removed the old diagram classes

--- Code/Data/ProductType.cs
+++
@@ -1,9 +0,0 @@
-﻿using Abc.Data.Common;
-
-namespace Abc.Data
-{
-    public class ProductType : NamedEntity
-    {
-        public ICollection<TypeOfProduct> TypeOfProduct { get; set; } = new List<TypeOfProduct>();
-    }
-}
