bed0444a743614db546d07fcdbc46e81e762238d
Rebecca Koljal <rebeck@taltech.ee>
2026-05-12 15:28:12 +0300

    Added ProductCategory class

---
+++ Code/Data/GroupProjectClasses/Rebecca/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿using Abc.Aids;
+
+namespace Abc.Data.GroupProjectClasses.Rebecca;
+
+public class ProductCategory
+{
+    [Select(typeof(ProductInfo))] public Guid? ProductId { get; set; }
+    [Select(typeof(Category))] public Guid? CategoryId { get; set; }
+    public ProductInfo Product { get; set; }
+    public Category Category { get; set; }
+}
