ecfafe1a54e8c21832e6a92fb50f19d04cea7a9f
Rebecca Koljal <rebeck@taltech.ee>
2026-05-17 17:12:16 +0300

    Removed problematic Maui and old unneeded folders

--- Soft/Todo/Client/TodoItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Abc.Soft.Todo.Client
-{
-    public class TodoItem
-    {
-        public string Title { get; set; } // cause nullable is disabled
-        public bool IsDone { get; set; }
-    }
-}
