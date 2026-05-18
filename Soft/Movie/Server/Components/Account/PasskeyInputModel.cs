85227b80d4722d65b379b338c9bd6e97e3e4cb97
Martin Kaaber <makaab@taltech.ee>
2026-05-18 10:57:02 +0300

    parandused

--- Soft/Movie/Server/Components/Account/PasskeyInputModel.cs
+++ Soft/Movie/Server/Components/Account/PasskeyInputModel.cs
@@ -2,7 +2,7 @@ namespace Abc.Soft.Web.Components.Account
 {
     public class PasskeyInputModel
     {
-        public string? CredentialJson { get; set; }
-        public string? Error { get; set; }
+        public string CredentialJson { get; set; }
+        public string Error { get; set; }
     }
 }
