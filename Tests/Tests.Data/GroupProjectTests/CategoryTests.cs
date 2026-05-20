7b49388108d0a7d1d8431f824f792dfd223ed4c6
Rebecca Koljal <rebeck@taltech.ee>
2026-05-20 11:18:58 +0300

    Made GroupProjectTests folder and CategoryTests

---
+++ Tests/Tests.Data/GroupProjectTests/CategoryTests.cs
@@ -0,0 +1,9 @@
+﻿using Abc.Data.GroupProjectClasses.Rebecca;
+using Abc.Tests.Aids;
+
+namespace Abc.Tests.Data.GroupProjectTests;
+
+[TestClass] public class CategoryTests : BaseTests<Category>
+{
+    [TestMethod] public void ParentCategoryIdTest() => IsProperty<Guid>(nameof(Category.ParentCategoryId));
+}
