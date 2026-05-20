9b0e882bc46fe4d2b4c97371a3cc185e5d8b36f0
Rebecca Koljal <rebeck@taltech.ee>
2026-05-20 11:27:46 +0300

    Made ProductCategoryTests

---
+++ Tests/Tests.Data/GroupProjectTests/ProductCategoryTests.cs
@@ -0,0 +1,12 @@
+﻿using Abc.Data.GroupProjectClasses.Rebecca;
+using Abc.Tests.Aids;
+
+namespace Abc.Tests.Data.GroupProjectTests;
+
+[TestClass] public class ProductCategoryTests : BaseTests<ProductCategory>
+{
+    [TestMethod] public void ProductIdTest() => IsProperty<Guid?>(nameof(ProductCategory.ProductId));
+    [TestMethod] public void CategoryIdTest() => IsProperty<Guid?>(nameof(ProductCategory.CategoryId));
+    [TestMethod] public void ProductTest() => IsProperty<ProductInfo>(nameof(ProductCategory.Product));
+    [TestMethod] public void CategoryTest() => IsProperty<Category>(nameof(ProductCategory.Category));
+}
